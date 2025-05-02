f2cff029a6c69c247ec70a96382686f8ede162f1
Mohamed Nacer <139363927+mohaemxd@users.noreply.github.com>
2025-05-02 14:22:12 +0100

    Updated Folder 2/5/25

---
+++ Project/categorie.js
@@ -0,0 +1,8 @@
+var categories = [
+    {name:"Fiction Books",}
+]
+
+function loadficCat(categorieName){
+    var catMainTitle = document.getElementById('categorieTitleName');
+    catMainTitle.innerText = categorieName;
+}
